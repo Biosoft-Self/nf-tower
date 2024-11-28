272ce5a8deb180d8a32a4d2e3af4143dc8ec8d2f
kozora <12146376+kozora@users.noreply.github.com>
2024-11-28 17:09:55 +0800

    del

--- tower-backend/README.md
+++
@@ -1,3 +0,0 @@
-```
-http://localhost:8000/api/workflow/list?max=30&offset=0
-```
--- tower-web/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  // apiUrl: "http://10.110.1.11:8088",
-  apiUrl: "/api",
-  version: "19.08.0",
-  commitId: "63a81b5.dirty",
-  captchaKey: null,
-};
